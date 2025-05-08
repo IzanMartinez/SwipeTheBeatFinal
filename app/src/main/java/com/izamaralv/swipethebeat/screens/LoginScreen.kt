e2ed8b2a20f9341db7081c3bf7cee39819f0d3dc
izanmartal <izanmartal@gmail.com>
2025-05-08 13:46:11 +0000

    feat: change login to firebase auth

--- app/src/main/java/com/izamaralv/swipethebeat/screens/LoginScreen.kt
+++ app/src/main/java/com/izamaralv/swipethebeat/screens/LoginScreen.kt
@@ -1,40 +1,48 @@
 package com.izamaralv.swipethebeat.screens
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.google.android.gms.tasks.Task
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.izamaralv.swipethebeat.R
 import com.izamaralv.swipethebeat.common.backgroundColor
-import com.izamaralv.swipethebeat.common.cardColor
 import com.izamaralv.swipethebeat.ui.components.HelpLink
 import com.izamaralv.swipethebeat.ui.theme.greenPastelColor
-import com.izamaralv.swipethebeat.utils.startSpotifyLogin
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginScreen(navController: NavHostController, modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var errorMessage by remember { mutableStateOf<String?>(null) }
 
 @Composable
 fun LoginScreen(navController: NavHostController, modifier: Modifier = Modifier) {
@@ -42,12 +50,11 @@ fun LoginScreen(navController: NavHostController, modifier: Modifier = Modifier)
     val systemUiController = rememberSystemUiController()
     systemUiController.setStatusBarColor(color = backgroundColor.value, darkIcons = false)
 
-    val context = LocalContext.current
     // Layout general de la pantalla
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = backgroundColor.value),
+            .background(color = backgroundColor.value).padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
@@ -60,18 +67,14 @@ fun LoginScreen(navController: NavHostController, modifier: Modifier = Modifier)
             // Logo
             Image(
                 painter = painterResource(id = R.drawable.border_logo_green),
-                contentDescription = "Logo",
-                modifier = Modifier
-                    .requiredWidth(412.dp)
-                    .requiredHeight(386.dp)
+                contentDescription = "Logo", modifier = Modifier.size(200.dp)
             )
             // Título
             Text(
                 text = "Swipe The Beat",
                 color = greenPastelColor,
                 textAlign = TextAlign.Center,
-                lineHeight = 3.5.em,
-                style = TextStyle(
+                lineHeight = 3.5.em, style = androidx.compose.ui.text.TextStyle(
                     fontSize = 40.sp
                 ),
                 modifier = Modifier
@@ -80,48 +83,61 @@ fun LoginScreen(navController: NavHostController, modifier: Modifier = Modifier)
             )
             Spacer(modifier = Modifier.height(1.dp)) // Espacio entre el título y el botón
         }
-
-        // Botón de inicio de sesión con Spotify
         Column(
             modifier = Modifier
-                .padding(bottom = 32.dp)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .clip(RoundedCornerShape(20.dp))
-                    .background(color = cardColor.value)
-                    .padding(horizontal = 25.dp, vertical = 10.dp)
-                    .clickable {
-                        startSpotifyLogin(context = context)
-                    }
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Email") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {
+                    val auth = FirebaseAuth.getInstance()
+                    auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task: Task<AuthResult> ->
+                            if (task.isSuccessful) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("Firebase Auth", "signInWithEmail:success")
+                                val user = auth.currentUser
+                                errorMessage = null
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("Firebase Auth", "signInWithEmail:failure", task.exception)
+                                errorMessage = "Authentication failed."
+                            }
+                        }
+                },
+                modifier = Modifier.fillMaxWidth()
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.spotify_logo),
-                    contentDescription = "Spotify Logo",
-                    modifier = Modifier
-                        .requiredWidth(41.dp)
-                        .requiredHeight(38.dp)
-                )
-                Spacer(modifier = Modifier.width(8.dp))
+                Text("Login")
+            }
+            // Display error message if there is one
+            if (errorMessage != null) {
                 Text(
-                    text = "Inicia sesión con Spotify",
-                    color = greenPastelColor,
-                    lineHeight = 8.75.em,
-                    style = TextStyle(fontSize = 16.sp)
+                    text = errorMessage!!,
+                    color = androidx.compose.ui.graphics.Color.Red,
+                    modifier = Modifier.padding(top = 8.dp)
                 )
             }
 
-            Spacer(modifier = Modifier.height(40.dp)) // Espacio entre el botón y el enlace de texto
-
-            // Composable para el enlace de ayuda
-            Row(modifier = modifier) {
-                HelpLink("¿Necesitas ayuda? Envia un correo a soporte")
-                Spacer(modifier = Modifier.height(40.dp))
-            }
+            Spacer(modifier = Modifier.height(16.dp))
         }
+        // Composable para el enlace de ayuda
+        HelpLink("¿Necesitas ayuda? Envia un correo a soporte")
     }
 }
+
