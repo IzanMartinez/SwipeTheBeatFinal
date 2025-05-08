e14415e40f6e14ba4323fdff3985cab2b60ec8ba
izanmartal <izanmartal@gmail.com>
2025-05-08 13:52:22 +0000

    feat: change app startup logic to check for firebase user

--- app/src/main/java/com/izamaralv/swipethebeat/MainActivity.kt
+++ app/src/main/java/com/izamaralv/swipethebeat/MainActivity.kt
@@ -1,34 +1,29 @@
 package com.izamaralv.swipethebeat
 
 import android.Manifest
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.izamaralv.swipethebeat.navigation.NavGraph
 import com.izamaralv.swipethebeat.ui.components.NotificationHelper
 import com.izamaralv.swipethebeat.ui.theme.SwipeTheBeatTheme
-import com.izamaralv.swipethebeat.utils.Credentials
-import com.izamaralv.swipethebeat.utils.ProfileManager
-import com.izamaralv.swipethebeat.utils.SpotifyManager
-import com.izamaralv.swipethebeat.utils.TokenManager
+import com.izamaralv.swipethebeat.utils.ProfileManagerFirebase
 import com.izamaralv.swipethebeat.viewmodel.InitializationViewModel
 import com.izamaralv.swipethebeat.viewmodel.ProfileViewModel
 
 class MainActivity : ComponentActivity() {
     // Declaración de variables para la navegación y gestión de Spotify y perfiles
-    private lateinit var navController: NavHostController
-    private lateinit var spotifyManager: SpotifyManager
-    private lateinit var profileManager: ProfileManager
+    private lateinit var navController:NavHostController
+    private lateinit var profileManager: ProfileManagerFirebase
     private val profileViewModel: ProfileViewModel by viewModels()
     private val initializationViewModel: InitializationViewModel by viewModels()
 
@@ -54,7 +49,6 @@ class MainActivity : ComponentActivity() {
         NotificationHelper.createNotificationChannel(this)
 
         // Inicializa los gestores
-        spotifyManager = SpotifyManager(applicationContext)
         profileManager = ProfileManager(applicationContext, profileViewModel)
 
         // Obtiene el perfil del usuario al iniciar
@@ -70,75 +64,31 @@ class MainActivity : ComponentActivity() {
                             navController = navController,
                             profileViewModel = profileViewModel,
                         )
-                        checkTokenAndNavigate()
+                        checkUserAndNavigate()
                     }
                 }
             }
         }
     }
 
-    private fun fetchUserProfileOnStart() {
-        val tokenManager = TokenManager(applicationContext)
-        val accessToken = tokenManager.getAccessToken()
-        val refreshToken = tokenManager.getRefreshToken()
-        val expiresIn = 3600
-
-        if (accessToken != null && refreshToken != null) {
-            // Inicializa el cliente de Spotify y obtiene el perfil del usuario
-            spotifyManager.initializeSpotifyClient(accessToken, refreshToken, expiresIn)
-            spotifyManager.initializeSongRepository(accessToken)
-            profileManager.fetchUserProfile(accessToken)
-            initializationViewModel.setInitialized()
-        } else {
-            // Inicia el flujo de autenticación OAuth si no hay tokens
-            initiateOAuthFlow()
-        }
-    }
-
-    private fun initiateOAuthFlow() {
-        val clientId = Credentials.CLIENT_ID
-        val redirectUri = Credentials.REDIRECT_URI
-        val authorizationUrl = spotifyManager.getAuthorizationUrl(clientId, redirectUri)
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(authorizationUrl))
-        startActivity(intent)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val intent = intent
-        intent?.data?.let { uri ->
-            if (uri.scheme == "myapp" && uri.host == "callback") {
-                val code = uri.getQueryParameter("code")
-                code?.let {
-                    spotifyManager.exchangeCodeForToken(it) { accessToken, refreshToken ->
-                        Log.d("MainActivity", "Access Token: $accessToken") // Registra el token también aquí
-                        spotifyManager.initializeSpotifyClient(accessToken, refreshToken, 3600)
-                        spotifyManager.initializeSongRepository(accessToken)
-                        profileManager.fetchUserProfile(accessToken)
-
-                        runOnUiThread {
-                            initializationViewModel.setInitialized()
-                            navController.navigate("main_screen") {
-                                popUpTo("login_screen") { inclusive = true }
-                            }
-                        }
+    private fun checkUserAndNavigate() {
+        lifecycleScope.launchWhenCreated {
+            val auth = FirebaseAuth.getInstance()
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                // User is logged in, go to main screen
+                navController.navigate("main_screen") {
+                    popUpTo("login_screen") { inclusive = true }
+                }
+            } else {
+                // No user logged in, go to login screen
+                navController.navigate("login_screen") {
+                    popUpTo(navController.graph.startDestinationId) {
+                        inclusive = true
                     }
                 }
+                initializationViewModel.setInitialized()
             }
         }
-    }
-
-    private fun checkTokenAndNavigate() {
-        val tokenManager = TokenManager(applicationContext)
-        val accessToken = tokenManager.getAccessToken()
-        if (accessToken != null) {
-            navController.navigate("main_screen") {
-                popUpTo("login_screen") { inclusive = true }
-            }
-        }
-    }
-
-    private fun logout() {
-        spotifyManager.logout(navController)
     }
 }
